0fb04151cae4c58bfab862841a267ea578c94e8a
hugoayed <hugoayed@gmail.com>
2021-06-14 23:21:28 +0200

    new page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navigation />
+    <Home />
+    <LeProjet />
+    <APropos />
+    <Footer />
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
+import Home from './components/Home.vue'
+import LeProjet from './components/LeProjet.vue'
+import APropos from './components/APropos.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation,
+    Home,
+    LeProjet,
+    APropos,
+    Footer,
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  min-width: fit-content;
 }
 </style>
